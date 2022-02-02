48b16a071d6334c76772678541a974714e6ff048
arshdeepdgreat <arshdeepdgreat@gmail.com>
2022-02-02 23:10:05 +0530

    Send response

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const app = express ()
 const https = require("https")
 
 app.get("/",function(req,res){
-    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=8d5f0776a113ac7e8805634ddbf789e4";
+    const url="https://api.openweathermap.org/data/2.5/weather?q=London&appid=8d5f0776a113ac7e8805634ddbf789e4&units=metric";
     https.get(url,function(response){
         console.log(response.statusCode);
         response.on("data",function(data){
             const weatherData=JSON.parse(data)
-            console.log(weatherData.main.temp)
+            console.log(weatherData.weather[0].description)
+            res.send("The temp is "+ weatherData.main.temp+" degrees Celcius")
         })
     })
-    res.send("Hello")
 })
 
 app.listen(3000,function(){
